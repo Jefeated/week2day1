d68730b678a5917ef5eaae4a35f9306d047214f1
Jefeated <jeffng@live.ca>
2017-03-13 23:42:33 +0000

    completed callback

---
+++ callback.js
@@ -0,0 +1,34 @@
+var https = require('https');
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step4.html'
+};
+
+function getHTML (options, callback) {
+
+  /* Add your code here */
+var chunk = '';
+  var options = {
+  host: requestOptions.host,
+  path: requestOptions.path
+  };
+
+  https.get(requestOptions, function(response){
+    response.setEncoding('utf8');
+    response.on('data', function(data) {
+      chunk += data;
+    });
+    response.on('end', function() {
+      console.log(chunk);
+    });
+    response.on('error', function(err) {
+      console.log('404');
+    });
+    printHTML(chunk);
+  });
+}
+
+function printHTML (html) {
+  console.log(html);
+}
+getHTML(printHTML);
